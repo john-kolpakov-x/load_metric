6b97758b9ea575258e3f5f4d5e98f188561d813f
pompei <john.kolpakov.x@gmail.com>
2016-09-10 21:51:15 +0600

    First init

---
+++ LoadMemory.cpp
@@ -0,0 +1,5 @@
+//
+// Created by pompei on 10.09.16.
+//
+
+#include "LoadMemory.h"
---
+++ LoadMemory.h
@@ -0,0 +1,14 @@
+//
+// Created by pompei on 10.09.16.
+//
+
+#ifndef LOAD_METRIC_LOADMEMORY_H
+#define LOAD_METRIC_LOADMEMORY_H
+
+
+class LoadMemory {
+  
+};
+
+
+#endif //LOAD_METRIC_LOADMEMORY_H
