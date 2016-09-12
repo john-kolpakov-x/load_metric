6a1849d234f7d36139fd4f0508b40f688b057360
pompei <john.kolpakov.x@gmail.com>
2016-09-12 21:08:41 +0600

    Killed warning

--- load_metric.c
+++ load_metric.c
@@ -4,7 +4,10 @@ static unsigned long long rdtsctime() {
   unsigned int eax, edx;
   unsigned long long val;
   __asm__ __volatile__("rdtsc": "=a"(eax), "=d"(edx));
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wuninitialized"
   val = edx;
+#pragma clang diagnostic pop
   val = val << 32;
   val += eax;
   return val;
